bf983f25389dba17a576c06eaec543a2113c0f15
Muhammad Lawan <mlawan252@gmail.com>
2024-08-07 15:26:54 +0100

    moved Form to app component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,45 @@
 import './App.css';
 import Mortgage from './components/Mortgage';
 import Result from './components/Result';
-
+import Form from "./components/Form";
+import { useState } from "react";
 export default function App() {
+  const [mortgageDetails, setMortgageDetails] = useState({
+    amount: "",
+    term: "",
+    rate: "",
+    type: "",
+  });
+  const [errors, setErrors] = useState({
+    amount: "",
+    term: "",
+    rate: "",
+    type: "",
+  });
+  function handleClearAll() {
+    setMortgageDetails({
+      amount: "",
+      term: "",
+      rate: "",
+      type: "",
+    });
+    handleClearError()
+  }
+
+  function handleClearError(){
+    setErrors({ amount: "", term: "", rate: "", type: "" });
+  }
   return (
     <div className="my-app">
-      <Mortgage />
+      <Mortgage onHandleClick={handleClearAll}>
+      <Form
+        setMortgageDetails={setMortgageDetails}
+        mortgageDetails={mortgageDetails}
+        errors={errors}
+        setErrors={setErrors}
+        onHandleClick={handleClearError}
+      />
+      </Mortgage>
       <Result />
     </div>
   );
--- src/components/Mortgage.js
+++ src/components/Mortgage.js
@@ -1,42 +1,15 @@
 import Button from "./Button";
-import Form from "./Form";
+
 import Header from "./Header";
-import { useState } from "react";
-export default function Mortgage() {
-  const [mortgageDetails, setMortgageDetails] = useState({
-    amount: "",
-    term: "",
-    rate: "",
-    type: "",
-  });
-  const [errors, setErrors] = useState({
-    amount: "",
-    term: "",
-    rate: "",
-    type: "",
-  });
-  function handleClick() {
-    setMortgageDetails({
-      amount: "",
-      term: "",
-      rate: "",
-      type: "",
-    });
-    setErrors({ amount: "", term: "", rate: "", type: "" });
-  }
+
+export default function Mortgage({ children, onHandleClick }) {
   return (
     <div className="mortgage">
       <div className="container">
-        <Header>Mortgage Calculator</Header>
-        <Button onHandleClick={handleClick}>Clear All</Button>
+        <Header >Mortgage Calculator</Header>
+        <Button onHandleClick={onHandleClick}>Clear All</Button>
       </div>
-      <Form
-        setMortgageDetails={setMortgageDetails}
-        mortgageDetails={mortgageDetails}
-        errors={errors}
-        setErrors={setErrors}
-        onHandleClick={handleClick}
-      />
+      {children}
     </div>
   );
 }
